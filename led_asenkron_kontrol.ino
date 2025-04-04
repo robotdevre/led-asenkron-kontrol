4046031f1de5802677b30a32c52a40935eed66e5
Uğur Kerim Sirke <robotdevre@gmail.com>
2025-04-04 23:41:13 +0300

    Create led_asenkron_kontrol.ino

---
+++ led_asenkron_kontrol.ino
@@ -0,0 +1,28 @@
+struct LedKontrol {
+  int pin;
+  unsigned long aralik;
+  unsigned long oncekiZaman;
+  bool durum;
+};
+
+LedKontrol led1 = {8, 500, 0, false};
+LedKontrol led2 = {9, 700, 0, false};
+
+void setup() {
+  pinMode(led1.pin, OUTPUT);
+  pinMode(led2.pin, OUTPUT);
+}
+
+void loop() {
+  unsigned long simdikiZaman = millis();
+  ledGuncelle(led1, simdikiZaman);
+  ledGuncelle(led2, simdikiZaman);
+}
+
+void ledGuncelle(LedKontrol &led, unsigned long simdikiZaman) {
+  if (simdikiZaman - led.oncekiZaman >= led.aralik) {
+    led.oncekiZaman = simdikiZaman;
+    led.durum = !led.durum;
+    digitalWrite(led.pin, led.durum);
+  }
+}
